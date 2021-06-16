f08df6b239d7570a9284b34fc8979669b4574846
Bala Raman <srbala@gmail.com>
2021-06-15 22:28:46 -0400

    Adding parameters to build.sh
    Signed-off-by: Bala Raman <srbala@gmail.com>

--- build.sh
+++ build.sh
@@ -5,11 +5,37 @@
 
 set -euo pipefail
 
+PARM='-t'
+TYPE='default'
+
+if [ "$#" == "0" ]; then
+    echo "No input parameter passed, processing 'default' build"
+else
+    if [ "$#" == "2" ]; then
+        PARM=$1
+        TYPE=$2 
+    else    
+        echo "Invalid input arguments. Use './build.sh -t default|minimal'"
+        exit 1
+    fi
+fi
+
+if [ "$PARM" != "-t" ]; then
+    echo "Invalid option; Use '-t' to specify build type"
+    exit 1
+fi
+
+if [ "$TYPE" != "default" ] && [ "$TYPE" != "minimal" ]; then
+    echo "Invalid type '$TYPE'; Use 'default' OR 'minimal' as build type"
+    exit 1
+fi
+
 RELEASE_VER='8'
 ARCH="$(uname -m)"
-IMAGE_NAME="almalinux-${RELEASE_VER}-docker.tar.xz"
-KS_PATH="./kickstarts/almalinux-${RELEASE_VER}.${ARCH}.ks"
+IMAGE_NAME="almalinux-${RELEASE_VER}-${TYPE}-docker.tar.xz"
+KS_PATH="./kickstarts/almalinux-${RELEASE_VER}-${TYPE}.${ARCH}.ks"
 OUTPUT_DIR="./result"
+DOCKER_FILE=${OUTPUT_DIR}/Dockerfile-${TYPE}
 
 
 if [[ -d "${OUTPUT_DIR}" ]]; then
@@ -20,12 +46,12 @@ fi
 
 livemedia-creator --no-virt --make-tar --ks "${KS_PATH}" \
                   --image-name="${IMAGE_NAME}" \
-                  --project "AlmaLinux OS ${RELEASE_VER} Docker" \
+                  --project "AlmaLinux OS ${RELEASE_VER}-${TYPE} Docker" \
                   --releasever "${RELEASE_VER}" \
                   --resultdir "${OUTPUT_DIR}"
 
 
-cat << EOF > "${OUTPUT_DIR}/Dockerfile"
+cat << EOF > "${DOCKER_FILE}"
 FROM scratch
 ADD ${IMAGE_NAME} /
 
