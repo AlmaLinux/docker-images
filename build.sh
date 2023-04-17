23e59a3d05509f933c531b51d249672b4803ff00
beholdenkey <justthered63@gmail.com>
2023-04-17 15:46:04 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -12,112 +12,110 @@ TYPE='default'
 
 OPTIND=1
 
-
 show_usage() {
-    echo -e 'Generates an AlmaLinux OS rootfs and Dockerfile\n'
-    echo -e 'Usage: build.sh [OPTION]...\n'
-    echo '  -h        show this message and exit'
-    echo '  -o        output directory path. Default is "./result"'
-    echo '  -t        image build type like "default", "init", "micro", "minimal". Default is "default"'
-    echo '  -v        AlmaLinux major release version, 8 or 9'
+	echo -e 'Generates an AlmaLinux OS rootfs and Dockerfile\n'
+	echo -e 'Usage: build.sh [OPTION]...\n'
+	echo '  -h        show this message and exit'
+	echo '  -o        output directory path. Default is "./result"'
+	echo '  -t        image build type like "default", "init", "micro", "minimal". Default is "default"'
+	echo '  -v        AlmaLinux major release version, 8 or 9'
 }
-
 
 save_logs() {
-    local -r logs_dir="${1}"
-    mkdir -p "${logs_dir}"
-    if [[ -d anaconda ]]; then
-        mv anaconda "${logs_dir}/"
-    fi
-    mv *.log "${logs_dir}/"
+	local -r logs_dir="${1}"
+	mkdir -p "${logs_dir}"
+	if [[ -d anaconda ]]; then
+		mv anaconda "${logs_dir}/"
+	fi
+	mv *.log "${logs_dir}/"
 }
 
-
 while getopts "ho:t:v:" opt; do
-    case "${opt}" in
-        h)
-            show_usage
-            exit 0
-            ;;
-        o)
-            OUTPUT_DIR="${OPTARG}"
-            ;;
-        t)
-            case "${OPTARG}" in
-                default|init|micro|minimal|base)
-                    TYPE="${OPTARG}"
-                    ;;
-                *)
-                    echo "Error: unsupported build type ${OPTARG}" 1>&2
-                    exit 1
-                    ;;
-            esac
-            ;;
-        v)
-            case "${OPTARG}" in
-                8|9)
-                    RELEASE_VER="${OPTARG}"
-                    ;;
-                *)
-                    echo "Error: unsupported version number ${OPTARG}" 1>&2
-                    exit 1
-                    ;;
-            esac
-            ;;
-        *)
-            exit 1
-            ;;
-    esac
+	case "${opt}" in
+	h)
+		show_usage
+		exit 0
+		;;
+	o)
+		OUTPUT_DIR="${OPTARG}"
+		;;
+	t)
+		case "${OPTARG}" in
+		default | init | micro | minimal | base)
+			TYPE="${OPTARG}"
+			;;
+		*)
+			echo "Error: unsupported build type ${OPTARG}" 1>&2
+			exit 1
+			;;
+		esac
+		;;
+	v)
+		case "${OPTARG}" in
+		8 | 9)
+			RELEASE_VER="${OPTARG}"
+			;;
+		*)
+			echo "Error: unsupported version number ${OPTARG}" 1>&2
+			exit 1
+			;;
+		esac
+		;;
+	*)
+		exit 1
+		;;
+	esac
 done
 
 OUTPUT_DIR="${OUTPUT_DIR}_${RELEASE_VER}_${ARCH}-${TYPE}"
 
-if [[ -d "${OUTPUT_DIR}" ]]; then
-    echo "Error: output directory ${OUTPUT_DIR} already exists, please remove it" 1>&2
-    exit 1
+if [[ -d ${OUTPUT_DIR} ]]; then
+	echo "Error: output directory ${OUTPUT_DIR} already exists, please remove it" 1>&2
+	exit 1
 fi
-
 
 IMAGE_NAME="almalinux-${RELEASE_VER}-docker-${ARCH}-${TYPE}.tar.xz"
 KS_PATH="./kickstarts/almalinux-${RELEASE_VER}-${TYPE}.ks"
 DOCKER_FILE=${OUTPUT_DIR}/Dockerfile
 
-
 livemedia-creator --no-virt --make-tar --ks "${KS_PATH}" \
-                  --image-name="${IMAGE_NAME}" \
-                  --project "AlmaLinux OS ${RELEASE_VER}-${TYPE} Docker" \
-                  --releasever "${RELEASE_VER}" \
-                  --resultdir "${OUTPUT_DIR}" \
-                  --anaconda-arg "--nosave all"
+	--image-name="${IMAGE_NAME}" \
+	--project "AlmaLinux OS ${RELEASE_VER}-${TYPE} Docker" \
+	--releasever "${RELEASE_VER}" \
+	--resultdir "${OUTPUT_DIR}" \
+	--anaconda-arg "--nosave all"
 
 # save list of packages installed
-jq .[] -r /tmp/dnf.cache/tempfiles.json | awk -F '/' '{print $5}' | sort > ${OUTPUT_DIR}/rpm-packages-${ARCH}-${TYPE}
+jq .[] -r /tmp/dnf.cache/tempfiles.json | awk -F '/' '{print $5}' | sort >"${OUTPUT_DIR}"/rpm-packages-"${ARCH}"-"${TYPE}"
 
-STARTCMD=$( cat <<EOF
+STARTCMD=$(
+	cat <<EOF
 
 CMD ["/bin/bash"]
 EOF
 )
 
-if [ ${TYPE} == 'init' ]; then
-  STARTCMD=$( cat <<EOF
+if [ "${TYPE}" == 'init' ]; then
+	STARTCMD=$(
+		cat <<EOF
 
 CMD ["/sbin/init"]
 
 STOPSIGNAL SIGRTMIN+3
 EOF
-)
+	)
 fi
 
-if [ ${TYPE} == 'micro' ]; then
-  STARTCMD=$( cat <<EOF
+if [ "${TYPE}" == 'micro' ]; then
+	STARTCMD=$(
+		cat <<EOF
 
 CMD ["/bin/sh"]
 EOF
-)
+	)
 fi
 
-cat << EOF > "${OUTPUT_DIR}/Dockerfile-${ARCH}-${TYPE}"
+cat <<EOF >"${OUTPUT_DIR}/Dockerfile-${ARCH}-${TYPE}"
 FROM scratch
 ADD ${IMAGE_NAME} /
 ${STARTCMD}
